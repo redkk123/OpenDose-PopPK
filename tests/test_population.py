9c78567cab3353e8d9f284c3746c42965699aad1
Angelo Gabriel <angelogabriel860@gmail.com>
2026-02-22 02:39:17 -0300

    add population simulation tests

---
+++ tests/test_population.py
@@ -0,0 +1,10 @@
+import numpy as np
+from opendose_poppk import PopulationSimulator
+
+def test_population_runs():
+
+    sim = PopulationSimulator()
+
+    result = sim.simulate(n=10)
+
+    assert len(result) == 10
